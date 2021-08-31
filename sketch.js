d9a1fa64895eab0e55357ce2ba4dd4392c724aa3
VIRTUALGOD325 <65289865+VIRTUALGOD325@users.noreply.github.com>
2021-08-31 17:28:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,45 +4,23 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var engine;
-
-var world;
-
-var throwButton;
-
-function preload()
-{
-	
-}
+var engine, world;
 
 function setup() {
 	createCanvas(800, 700);
 
-	ball=createSprite(width/2, 80, 10,10);
-
 	engine = Engine.create();
 	world = engine.world;
 
+	paper = new Paper (100, 600, 10);
 
+	ground = new Ground (400, 680, 800, 20);
 
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
-
-	dropGround = createSprite(width/1.65, height-80, 20,100);
-	dropGround.shapeColor = color("red");
-	dropGroundside1 = createSprite(width/2, height-50,150,20);
-	dropGroundside1 = color("red");
-	dropGround = createSprite(width/2.5, height-80, 20,100);
-	dropGround.shapeColor = color("red");
-
-	World.add(world, ball);
-
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
-
+	leftSide = new DustBin (550, 620, 100, 20);
+	bottomSide = new DustBin (610, 660, 20, 100)
+	rightSide = new DustBin (670, 620, 100, 20);
 
 	Engine.run(engine);
-
   
 }
 
@@ -51,14 +29,23 @@ function draw() {
   rectMode(CENTER);
   background(0);
 
-  if(mousePressedOver(ball)){
-	  ball.velocityY = 3;
-  }
+  Engine.update(engine);
+
+  paper.display();
+
+  ground.display();
+
+  leftSide.display();
+  bottomSide.display();
+  rightSide.display();
 
   drawSprites();
- 
+
 }
 
-
-
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper.body, paper.body.position, {x:15,y: -15})
+	}
+}
 
